166d7b884faa799b289dcb17078e685051eb7027
Geovanny Silva <geovanny@eutsiv.com>
2023-01-02 08:52:30 -0300

    making fragments and trust simpler

--- src/Hyperscript.ts
+++ src/Hyperscript.ts
@@ -6,17 +6,13 @@ import {
 import { Component } from './Component'
 
 
-const fragments = (...nodes: Array<any>): VNode => {
-
-  return {
-    __sv__: true,
-    type: VNodeType.Fragment,
-    item: '',
-    attrs: {},
-    children: normalizeChildren(nodes)
-  }
-
-}
+const fragments = (...nodes: Array<any>): VNode => ({
+  __sv__: true,
+  type: VNodeType.Fragment,
+  item: '',
+  attrs: {},
+  children: normalizeChildren(nodes)
+})
 
 const hyperscript = (item: Component<any> | string, ...args: Array<any>): VNode => {
 
@@ -33,17 +29,13 @@ const hyperscript = (item: Component<any> | string, ...args: Array<any>): VNode 
 
 }
 
-const trust = (html: string = ""): VNode => {
-
-  return {
-    __sv__: true,
-    type: VNodeType.Raw,
-    item: html,
-    attrs: {},
-    children: []
-  }
-
-}
+const trust = (html: string = ''): VNode => ({
+  __sv__: true,
+  type: VNodeType.Raw,
+  item: html,
+  attrs: {},
+  children: []
+})
 
 export {
   fragments,
