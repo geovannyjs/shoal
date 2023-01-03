6111aaac72de870ecbc2484e6cf3e508cbcca9d7
Geovanny Silva <geovanny@eutsiv.com>
2023-01-03 13:32:24 -0300

    declaring create nodes functions

---
+++ src/Render.d.ts
@@ -0,0 +1,3 @@
+import { Component } from './Component';
+declare const render: (root: HTMLElement, component: Component<any>) => void;
+export { render };
--- src/Render.ts
+++ src/Render.ts
@@ -1,12 +1,36 @@
 import { Component } from './Component'
 import { hyperscript as h } from './Hyperscript'
-import { VNode } from './VNode'
+import { VNode, Type as VNodeType } from './VNode'
 
 
 type HTMLElementVNodes = HTMLElement & { vnodes: Array<VNode> }
 
+const createNodes = (parent: HTMLElementVNodes, cur: Array<VNode>):void => cur.forEach(vnode => vnode && createNode(parent, vnode))
+
+const createNode = (parent: HTMLElementVNodes, vnode: VNode):void => {
+  const dispatcher = {
+    [VNodeType.Component]: createNodeComponent,
+    [VNodeType.Fragment]: createNodeFragment,
+    [VNodeType.Raw]: createNodeRaw,
+    [VNodeType.Tag]: createNodeTag,
+    [VNodeType.Text]: createNodeText
+  }
+  dispatcher[vnode.type](parent, vnode)
+}
+
+const createNodeComponent = (parent: HTMLElementVNodes, vnode: VNode):void => {}
+
+const createNodeFragment = (parent: HTMLElementVNodes, vnode: VNode):void => {}
+
+const createNodeRaw = (parent: HTMLElementVNodes, vnode: VNode):void => {}
+
+const createNodeTag = (parent: HTMLElementVNodes, vnode: VNode):void => {}
+
+const createNodeText = (parent: HTMLElementVNodes, vnode: VNode):void => {}
+
 const updateNodes = (parent: HTMLElementVNodes, old: Array<VNode>, cur: Array<VNode>): void => {
   if (old === cur) return
+  else if (old.length === 0) createNodes(parent, cur)
 }
 
 const render = (root: HTMLElement, component: Component<any>) => {
