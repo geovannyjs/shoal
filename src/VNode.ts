a95271c8194494063299d14977c7993ece931546
Geovanny Silva <geovanny@eutsiv.com>
2023-01-05 10:14:20 -0300

    normalizeChildren simpler

--- src/VNode.ts
+++ src/VNode.ts
@@ -26,9 +26,7 @@ const normalize = (node: any): VNode => {
   return pure({ __sv__: true, type: Type.Text, item: String(node), attrs: pure(), children: [] })
 }
 
-const normalizeChildren = (nodes: Array<any>): Array<VNode> => {
-  return nodes.map(normalize)
-}
+const normalizeChildren = (nodes: Array<any>): Array<VNode> => nodes.map(normalize)
 
 export {
   normalizeChildren,
