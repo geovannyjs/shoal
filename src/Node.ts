d7b92f0b9234ea69b8923d506699eae8641658b2
Geovanny Silva <geovanny@eutsiv.com>
2023-01-24 10:35:03 -0300

    node type enum

---
+++ src/Node.ts
@@ -0,0 +1,13 @@
+enum Type {
+  Element = 1,
+  Attribute = 2,
+  Text = 3,
+  CDataSection = 4,
+  ProcessingInstruction = 7,
+  Comment = 8,
+  Document = 9,
+  DocumentType = 10,
+  Fragment = 11
+}
+
+export { Type }
